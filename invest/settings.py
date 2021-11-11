f003b317f5aa24b3f6b5de92bed0576d7f10f81d
unknown <joshijax08@gmail.com>
2021-11-11 17:52:43 +0100

    make it better

--- invest/settings.py
+++ invest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j8jbxn*)8vf(!w_3id@t7q#=ycl=h3rh5d3#ats-fn0%kdo+a$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['walletto.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['walletto.herokuapp.com','127.0.0.1', 'globefxtrade.herokuapp.com']
 
 
 # Application definition
